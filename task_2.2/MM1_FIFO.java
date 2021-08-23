62e1fef4f5433a43e7bd21603cd44a3d09caf01f
KikiC99 <86603073+KikiC99@users.noreply.github.com>
2021-08-23 17:56:15 +0100

    Add files via upload
    MM1 voll kommentiert. Fehlen noch die Statistiken mean und max Lagerzeit. Bei 'Q' habe ich auf Zeile 178 etwas hinzugefügt. Für LIFO wird removeLast anstatt removeFirst (wie bei tryTake) benutzt, damit man FIFO und LIFO benutzen kann

--- task_2.2/MM1_FIFO.java
+++ task_2.2/MM1_FIFO.java
@@ -19,12 +19,13 @@ import jasima.core.simulation.generic.Q;
 import jasima.core.util.ConsolePrinter;
 
 public class MM1_FIFO {
-	private static boolean consoleLog = true;
+	private static boolean consoleLog = true; //log printed in console for troubleshooting
 	
+	private static boolean FIFO = true; //if false, LIFO
 	private static final double LENGTH_SIM = 100.0; //duration of the simulation in minutes
 	private static final int NUM_JOB_TYPES = 10; //number of different types of jobs
 	private static final double MEAN_TIME = 10.0; //average time in minutes for both jobs and services
-	private static final int RANDOM_FACTORY_SEED = 23; //seed used for the simulation's random factory
+	private static final int RANDOM_FACTORY_SEED = 28; //seed used for the simulation's random factory
 	
 	static class MM1 extends SimComponentBase {
 
@@ -36,9 +37,9 @@ public class MM1_FIFO {
 		 * Once a service finds a job of the same time it immediately services it, without any processing time.
 		 * If a service does not find a matching job upon arrival it waits for one to be created.
 		 * */
-		private DblSequence meanArrival; //random sequence of doubles for the arrival time of jobs
-		private DblSequence meanService; //random sequence of doubles for the arrival time of services
-		private IntSequence typeArrival; //random sequence of integers for the type of job
+		private DblSequence arrivalJob; //random sequence of doubles for the arrival time of jobs
+		private DblSequence arrivalService; //random sequence of doubles for the arrival time of services
+		private IntSequence typeJob; //random sequence of integers for the type of job
 		private IntSequence typeService; //random sequence of integers for the type of service
 		
 		/* There are two queue arrays, each with, in this case, 10 queues. 
@@ -56,20 +57,25 @@ public class MM1_FIFO {
 		@Override
 		public void init() {
 			super.init();
-			meanArrival = initRndGen(new DblExp(MEAN_TIME), "arrivals"); //random sequence for jobs is initialized
-			meanService = initRndGen(new DblExp(MEAN_TIME), "services"); //random sequence for services is initialized
-			typeArrival = initRndGen(new IntUniformRange(0, NUM_JOB_TYPES - 1), "typeArrivals");
+			
+			//random sequences for types and arrival times are initialized
+			arrivalJob = initRndGen(new DblExp(MEAN_TIME), "jobs");
+			arrivalService = initRndGen(new DblExp(MEAN_TIME), "services");
+			typeJob = initRndGen(new IntUniformRange(0, NUM_JOB_TYPES - 1), "typeJobs");
 			typeService = initRndGen(new IntUniformRange(0, NUM_JOB_TYPES - 1), "typeServices");
-					
-			for(int i = 0; i < NUM_JOB_TYPES; i++) { //both queues are initialized
+			
+			 //both queues are initialized
+			for(int i = 0; i < NUM_JOB_TYPES; i++) {
 				qJob[i] = new Q<>();
 				qService[i] = new Q<>();
 			}
 			
-			scheduleIn(meanArrival.nextDbl(), getSim().currentPrio(), this::nextJob); //first job is scheduled
-			scheduleIn(meanService.nextDbl(), getSim().currentPrio(), this::nextService); //first service is scheduled
+			//the first job and first service are scheduled
+			scheduleIn(arrivalJob.nextDbl(), getSim().currentPrio(), this::nextJob);
+			scheduleIn(arrivalService.nextDbl(), getSim().currentPrio(), this::nextService);
 		}
 		
+		//all collected statistics are added to the map res to later be printed
 		@Override
 		public void produceResults(Map<String, Object> res) {
 			super.produceResults(res);
@@ -78,6 +84,7 @@ public class MM1_FIFO {
 			res.put("numServicedJobs", countServicedJobs);
 		}
 		
+		//method to simplify the printing of text in the console
 		void prtLine(String line) {
 			if (consoleLog == true) {
 				System.out.println(line);
@@ -85,51 +92,73 @@ public class MM1_FIFO {
 		}
 		
 		void nextJob() {
-			int qNmr = typeArrival.nextInt();
-			int jobID = countJob++;
+			int qNmr = typeJob.nextInt(); //the new job's type is randomly defined
+			int jobID = countJob++; //the job's ID is saved and the job counter increased by one
 			prtLine("new job     " + jobID + " type " + qNmr + " at " + getSim().simTime());
-			qJob[qNmr].tryPut(jobID);
-			if (qService[qNmr].numItems() > 0) {
-				int service = qService[qNmr].tryTake();
+			qJob[qNmr].tryPut(jobID); //the job is added to the corresponding queue
+			/* For every job queue there is a corresponding service queue. Every new job looks in it's service queue
+			 * for a waiting service, based on the job's type. If it finds a service, the service is removed from the
+			 * queue and the servicing process is immediately started. If the job does not find a waiting service
+			 * nothing further needs to be done. Finally the next job is scheduled at a random time.
+			 */
+			if (qService[qNmr].numItems() > 0) { //waiting service is found in corresponding queue
+				int service = qService[qNmr].tryTake(); //service is removed from queue
 				prtLine("			waiting service " + service + " found");
-				serviceProcess(qNmr);
+				serviceProcess(qNmr); //the job's servicing process is started
 			} else {
 				prtLine("			no waiting service found");
 			}
-			scheduleIn(meanArrival.nextDbl(), getSim().currentPrio(), this::nextJob);
+			scheduleIn(arrivalJob.nextDbl(), getSim().currentPrio(), this::nextJob); //the next job is scheduled
 		}
 
 		void nextService() {
-			int qNmr = typeService.nextInt();
-			int serviceID = countService++;
+			int qNmr = typeService.nextInt(); //the new service's type is randomly defined
+			int serviceID = countService++; //the service's ID is saved and the service counter increased by one
 			prtLine("new service " + serviceID + " type " + qNmr + " at " + getSim().simTime());
-			if (qJob[qNmr].numItems() > 0) {
+			/* Every new service looks in the queue corresponding to it's type for a waiting job. If it finds one the
+			 * servicing process is immediately initialized. If the service does not find a waiting queue then it adds
+			 * itself to the correct service queue and waits to be called up by a future job. Finally the next service
+			 * is scheduled at a random time. 
+			 */
+			if (qJob[qNmr].numItems() > 0) { //waiting job is found in corresponding queue
 				prtLine("			waiting job found");
-				serviceProcess(qNmr);
+				serviceProcess(qNmr); //the job's servicing process is started
 			} else {
 				prtLine("			service added to queue");
-				qService[qNmr].tryPut(countService);
+				qService[qNmr].tryPut(countService); //the service is added to the corresponding queue
 			}
-			scheduleIn(meanService.nextDbl(), getSim().currentPrio(), this::nextService); //()->nextService(xyz))
+			scheduleIn(arrivalService.nextDbl(), getSim().currentPrio(), this::nextService); //the next service is scheduled
 		}
 		
 		void serviceProcess(int qNmr) {
-			int job = qJob[qNmr].tryTake();
+			int job;
+			//depending if FIFO or LIFO is active the right take call is used and the job removed from the queue 
+			if (FIFO == true) {
+				job = qJob[qNmr].tryTake();
+			} else {
+				job = qJob[qNmr].tryTakeLast();
+			}
 			prtLine("			job " + job + " processed");
-			countServicedJobs++;
+			countServicedJobs++; //the counter for serviced jobs is increased by one
 		}
 	}
 	
 	@Test
 	public void testEventOriented() {
 		
-		Simulation sim = new Simulation();
-		sim.setSimulationLength(LENGTH_SIM);
+		Simulation sim = new Simulation(); //new simulation is created
+		sim.setSimulationLength(LENGTH_SIM); //the simulation's length is set
+		/* A random factory is needed to create the random number sequences for the types and arrival times of jobs
+		 * and services. The previously defined random seed is used.
+		 */
 		RandomFactory rf = new RandomFactory(sim, RANDOM_FACTORY_SEED);
 		sim.setRndStreamFactory(rf);
 		sim.addComponent(new MM1());
-		Map<String, Object> res = sim.performRun();
+		/* All the statistics produced and at the end returned by the simulation and saved in the map res to later
+		 * be printed in the console
+		 */
+		Map<String, Object> res = sim.performRun(); //the simulation is run and the returned statistics saved in res
 
-		ConsolePrinter.printResults(null, res);
+		ConsolePrinter.printResults(null, res); //the statistics saved in res are printed in the console
 	}
 }
---
+++ task_2.2/Q.java
@@ -0,0 +1,262 @@
+package jasima.core.simulation.generic;
+
+import static jasima.core.simulation.SimContext.currentProcess;
+import static jasima.core.simulation.SimContext.trace;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.Nullable;
+
+import jasima.core.simulation.SimContext;
+import jasima.core.simulation.SimProcess;
+import jasima.core.simulation.SimProcess.MightBlock;
+import jasima.core.simulation.SimProcess.ProcessState;
+import jasima.core.simulation.generic.Q.QEvent;
+import jasima.core.util.observer.Notifier;
+import jasima.core.util.observer.NotifierImpl;
+import jasima.core.util.observer.NotifierListener;
+
+public class Q<T> implements Notifier<Q<T>, QEvent> {
+
+	public static void enter(Q<SimProcess<?>> q) throws MightBlock {
+		SimProcess<?> proc = SimContext.currentProcess();
+		q.put(proc);
+	}
+
+	public static void leave(Q<SimProcess<?>> q) {
+		SimProcess<?> proc = SimContext.currentProcess();
+		boolean removed = q.remove(proc);
+		if (!removed) {
+			throw new IllegalStateException();
+		}
+	}
+
+	public static <T> QListener<T> traceQEvents(Q<T> q1) {
+		return q1.addListener(new QListener<T>() {
+
+			@Override
+			public void itemAdded(Q<T> q, T item) {
+				addTraceEntry("queue.added", q, item);
+			}
+
+			@Override
+			public void itemRemoved(Q<T> q, T item) {
+				addTraceEntry("queue.removed", q, item);
+			}
+
+			@Override
+			public void handleOther(Q<T> q, QEvent event) {
+				addTraceEntry("queue.other", q, event);
+			}
+
+			private void addTraceEntry(String event, Q<?> q, Object item) {
+				trace(event, item, q, q.numItems(), q.numWaitingPut(), q.numWaitingTake());
+			}
+
+		});
+	}
+
+	public static interface QEvent {
+	}
+
+	public enum QEvents implements QEvent {
+		ITEM_ADDED, ITEM_REMOVED,
+	}
+
+	public interface QListener<T> extends NotifierListener<Q<T>, QEvent> {
+		@Override
+		default void inform(Q<T> q, QEvent event) {
+			if (event == QEvents.ITEM_ADDED) {
+				itemAdded(q, q.lastAdded);
+			} else if (event == QEvents.ITEM_REMOVED) {
+				itemRemoved(q, q.lastRemoved);
+			} else {
+				handleOther(q, event);
+			}
+		}
+
+		default void itemAdded(Q<T> q, T item) {
+		}
+
+		@FunctionalInterface
+		interface ItemAdded<T> extends QListener<T> {
+			@Override
+			void itemAdded(Q<T> q, T item);
+		}
+
+		default void itemRemoved(Q<T> q, T item) {
+		}
+
+		@FunctionalInterface
+		interface ItemRemoved<T> extends QListener<T> {
+			@Override
+			void itemRemoved(Q<T> q, T item);
+		}
+
+		default void handleOther(Q<T> q, QEvent event) {
+		}
+	}
+
+// parameters
+
+	private int capacity = -1;
+	private String name = null;
+
+	// fields used during run
+
+	private Deque<T> items = new ArrayDeque<>();
+	private List<SimProcess<?>> awaitingTake = new ArrayList<>();
+	private List<SimProcess<?>> awaitingPut = new ArrayList<>();
+
+	// event notification
+
+	private T lastAdded = null, lastRemoved = null;
+	private NotifierImpl<Q<T>, QEvent> notifierImpl = new NotifierImpl<>(this);
+
+	public Q() {
+		super();
+	}
+
+	public Q(String name) {
+		this();
+		setName(name);
+	}
+
+	public void put(T t) throws MightBlock {
+		SimProcess<?> p = currentProcess();
+
+		awaitingPut.add(p);
+		while (numAvailable() <= 0) {
+			p.suspend();
+		}
+		awaitingPut.remove(p);
+
+		boolean putRes = tryPut(t);
+		assert putRes;
+	}
+
+	public boolean tryPut(T t) {
+		if (numAvailable() <= 0) {
+			return false;
+		}
+
+		items.addLast(Objects.requireNonNull(t));
+		itemAdded(t);
+
+		return true;
+	}
+
+	public T take() throws MightBlock {
+		SimProcess<?> p = currentProcess();
+
+		awaitingTake.add(p);
+		while (numItems() == 0) {
+			p.suspend();
+		}
+		awaitingTake.remove(p);
+
+		T res = tryTake();
+		assert res != null;
+
+		return res;
+	}
+
+	public @Nullable T tryTake() {
+		while (numItems() == 0) {
+			return null;
+		}
+
+		T res = items.removeFirst();
+		itemRemoved(res);
+		return res;
+	}
+	
+	public @Nullable T tryTakeLast() { //Henrique
+		while (numItems() == 0) {
+			return null;
+		}
+
+		T res = items.removeLast();
+		itemRemoved(res);
+		return res;
+	}
+
+	public boolean remove(T t) {
+		boolean res = items.remove(t);
+		if (res) {
+			itemRemoved(t);
+		}
+		return res;
+	}
+
+	private void itemAdded(T t) {
+		lastAdded = t;
+		fire(QEvents.ITEM_ADDED);
+		resumeTakeProcesses();
+	}
+
+	private void itemRemoved(T t) {
+		lastRemoved = t;
+		fire(QEvents.ITEM_REMOVED);
+		resumePutProcesses();
+	}
+
+	private void resumeTakeProcesses() {
+		awaitingTake.stream().filter(p -> p.processState() == ProcessState.PASSIVE).forEach(SimProcess::resume);
+	}
+
+	private void resumePutProcesses() {
+		awaitingPut.stream().filter(p -> p.processState() == ProcessState.PASSIVE).forEach(SimProcess::resume);
+	}
+
+	public int numItems() {
+		return items.size();
+	}
+
+	public int numAvailable() {
+		return (capacity < 0) ? Integer.MAX_VALUE : Math.max(capacity - items.size(), 0);
+	}
+
+	public int numWaitingTake() {
+		return awaitingTake.size();
+	}
+
+	public int numWaitingPut() {
+		return awaitingPut.size();
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int newCapacity) {
+		int oldAvailable = numAvailable();
+		capacity = newCapacity;
+		if (oldAvailable < numAvailable()) {
+			resumePutProcesses();
+		}
+	}
+
+	@Override
+	public Notifier<Q<T>, QEvent> notifierImpl() {
+		return notifierImpl;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return getName() != null ? getName() : super.toString();
+	}
+
+}
